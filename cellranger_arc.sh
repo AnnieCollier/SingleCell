7c3dc6f3989f170fa141de6d43039f3cc716dd64
AnnieCollier <90862478+AnnieCollier@users.noreply.github.com>
2022-01-20 17:08:38 -0800

    cellranger_arc.sh
    align multiome data

---
+++ cellranger_arc.sh
@@ -0,0 +1,5 @@
+/path/to/cellranger/cellranger-arc-2.0.0/cellranger-arc count --id=counts_multiome \
+                    --reference=/path/to/refdata-cellranger-arc-GRCh38-2020-A-2.0.0 \
+                    --libraries=/path/to/libraries.csv \
+                    --localcores=16 \
+                    --localmem=64
